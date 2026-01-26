bf711cc7c692c947c9fe556ad620a591ecf99e05
PARK YOUNG IL <zetooo1972@gmail.com>
2026-01-26 15:20:07 +0700

    package update

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-137dedbd'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.cd3o590defg"
+    "revision": "0.nvltdnq9b78"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
