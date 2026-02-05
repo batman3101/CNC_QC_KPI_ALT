85644946e60401e066d22368185e3b5aa0947e24
PARK YOUNG IL <zetooo1972@gmail.com>
2026-02-05 09:07:34 +0700

    검사자 등록 오류 수정

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-137dedbd'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.ces9itv3qug"
+    "revision": "0.m0mq044nru"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
